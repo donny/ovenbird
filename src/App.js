d5e5e6ea91d97ddfc268e11294d99a76f485651b
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-04-11 16:39:50 +1000

    Add initial nav overlay

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import Melbourne from "./Melbourne";
+import './App.css';
 
 class App extends Component {
   render() {
     return (
         <div>
+          <nav className="nav-overlay">
+            <div className="nav-overlay-content">
+              <a href="#">Hello1</a>
+              <a href="#">Hello2</a>
+            </div>
+          </nav>
           <Melbourne/>
         </div>
     );
